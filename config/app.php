1d271763f9389e0e6c16a74a4ecc8e1a937d2292
tiendungkid <phantiendung29111999@gmail.com>
2021-01-26 17:11:45 +0700

    Update gitignore

--- config/app.php
+++ config/app.php
@@ -161,6 +161,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        DoubleC\LaravelShopifyServiceProvider::class,
 
         /*
          * Package Service Providers...
