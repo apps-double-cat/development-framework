4c52c8f09692f1a3b1774d86be295b6252a2b3b1
Tiendungkid <phantiendung29111999@gmail.com>
2021-02-25 23:56:24 +0700

    Complete install flow

--- app/Http/Controllers/DevelopmentController.php
+++ app/Http/Controllers/DevelopmentController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
-use Illuminate\Http\Request;
 
 class DevelopmentController extends Controller
 {
@@ -15,7 +14,6 @@ class DevelopmentController extends Controller
      */
     public function index(): Factory|View|Application
     {
-        dd(auth()->user());
         return view('welcome');
     }
 }
