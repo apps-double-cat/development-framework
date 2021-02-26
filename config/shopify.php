1c563e734e081a6d14b7b76a1ddfa5d1e470cb2e
tiendungkid <phantiendung29111999@gmail.com>
2021-02-26 17:20:12 +0700

    Update install app

--- config/shopify.php
+++ config/shopify.php
@@ -4,5 +4,7 @@ return [
     'api_key'               => env('SHOPIFY_API_KEY'),
     'shared_secret'         => env('SHOPIFY_SHARED_SECRET'),
     'redirect_url'          => env('SHOPIFY_REDIRECT_URL'),
-    'permissions'           => [env('SHOPIFY_PERMISSIONS')]
+    'permissions'           => [env('SHOPIFY_PERMISSIONS')],
+    'api_version'           => '2020-01',
+    'app_url'               => env('APP_URL')
 ];
