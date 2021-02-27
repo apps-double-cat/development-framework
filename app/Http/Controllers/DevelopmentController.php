485addb155e8ae99a6c7b78c0b7f2cfe4523e251
tiendungkid <phantiendung29111999@gmail.com>
2021-02-27 12:16:16 +0700

    Update migration & uninstall app

--- app/Http/Controllers/DevelopmentController.php
+++ app/Http/Controllers/DevelopmentController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use DoubleC\LaravelShopify\Traits\InstalledShop;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
 
 class DevelopmentController extends Controller
 {
+    use InstalledShop;
+
     /**
      * Home page
      * @return Factory|View|Application
