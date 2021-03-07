53b1ac43e6527d6ce89c6d0f034a24a31c8dbe87
Tiendungkid <phantiendung29111999@gmail.com>
2021-03-07 16:45:39 +0700

    Update development

--- app/Http/Controllers/DevelopmentController.php
+++ app/Http/Controllers/DevelopmentController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use DoubleC\LaravelShopify\Facades\ShopSetting;
 use DoubleC\LaravelShopify\Traits\InstalledShop;
+use DoubleC\LaravelShopify\Traits\ShopSubscription;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
@@ -12,13 +13,15 @@ use function PHPUnit\Framework\callback;
 class DevelopmentController extends Controller
 {
     use InstalledShop;
+    use ShopSubscription;
 
     /**
      * Home page
-     * @return Factory|View|Application
+//     * @return Factory|View|Application
      */
-    public function index(): Factory|View|Application
+    public function index()
     {
+        //return $this->shopSubscribe(2);
         return view('welcome');
     }
 }
--- config/shopify.php
+++ config/shopify.php
@@ -6,5 +6,9 @@ return [
     'redirect_url'          => env('SHOPIFY_REDIRECT_URL'),
     'permissions'           => [env('SHOPIFY_PERMISSIONS')],
     'api_version'           => '2020-01',
-    'app_url'               => env('APP_URL')
+    'app_url'               => env('APP_URL'),
+    # Free available => 0 | Paid app: 1
+    'app_type'              => 1,
+    # Auto apply plan if app has free plan
+    'default_plan_id'       => 1
 ];
