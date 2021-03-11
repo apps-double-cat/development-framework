ff592bd57906636a03b850c610c6c31b508fe620
tiendungkid <phantiendung29111999@gmail.com>
2021-03-11 17:15:15 +0700

    Update config

--- config/shopify.php
+++ config/shopify.php
@@ -1,14 +1,13 @@
 <?php
 return [
-    'app_name'              => env('APP_NAME'),
-    'api_key'               => env('SHOPIFY_API_KEY'),
-    'shared_secret'         => env('SHOPIFY_SHARED_SECRET'),
-    'redirect_url'          => env('SHOPIFY_REDIRECT_URL'),
-    'permissions'           => [env('SHOPIFY_PERMISSIONS')],
-    'api_version'           => '2020-01',
-    'app_url'               => env('APP_URL'),
-    # Free available => 0 | Paid app: 1
-    'app_type'              => 1,
-    # Auto apply plan if app has free plan
-    'default_plan_id'       => 1
+    'app_name'                  => env('APP_NAME'),
+    'api_key'                   => env('SHOPIFY_API_KEY'),
+    'shared_secret'             => env('SHOPIFY_SHARED_SECRET'),
+    'redirect_url'              => env('SHOPIFY_REDIRECT_URL'),
+    'permissions'               => [env('SHOPIFY_PERMISSIONS')],
+    'api_version'               => '2020-01',
+    'app_url'                   => env('APP_URL'),
+    'activated_redirect_link'   => '/',
+    'app_type'                  => 1,
+    'default_plan_id'           => 1
 ];
