fa5e470f7a2725f2ce57723529a44b498086c837
Tiendungkid <phantiendung29111999@gmail.com>
2021-04-01 00:03:55 +0700

    Update pricing & remove resource

--- app/Http/Controllers/DevelopmentController.php
+++ app/Http/Controllers/DevelopmentController.php
@@ -2,26 +2,20 @@
 
 namespace App\Http\Controllers;
 
-use DoubleC\LaravelShopify\Facades\ShopSetting;
-use DoubleC\LaravelShopify\Traits\InstalledShop;
 use DoubleC\LaravelShopify\Traits\ShopSubscription;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
-use function PHPUnit\Framework\callback;
 
 class DevelopmentController extends Controller
 {
-    use InstalledShop;
     use ShopSubscription;
 
     /**
-     * Home page
-//     * @return Factory|View|Application
+     * @return Application|Factory|View
      */
-    public function index()
+    public function index(): Factory|View|Application
     {
-        //return $this->shopSubscribe(2);
         return view('welcome');
     }
 }
