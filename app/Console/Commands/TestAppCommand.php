18c1d018506466977d5a9c295af94204cefb085f
tiendungkid <phantiendung29111999@gmail.com>
2021-04-03 09:20:13 +0700

    hotfix typo SlackNotification

--- app/Console/Commands/TestAppCommand.php
+++ app/Console/Commands/TestAppCommand.php
@@ -13,28 +13,6 @@ class TestAppCommand extends Command
 
     public function handle(): void
     {
-        # Log with default content debug
-        dc_slack('This is debug mode');
-        # Log with color
-        dc_slack()->debug('DEBUG');
-        dc_slack()->info('INFO');
-        dc_slack()->warning('WARNING');
-        dc_slack()->error('ERROR');
-        # With title
-        dc_slack()->title('This is title')->warning('THIS IS VERY IMPORTANT');
-        # With exception
-        try {
-            # Some code wrong
-        } catch (Exception $exception) {
-            dc_slack()->exception($exception);
-            # OR
-            dc_slack()->title("THIS IS BUG")->exception($exception);
-        }
-        # With array | object
-        $a = [1, 2, 3];
-        dc_slack()->array($a);
-        $b = new stdClass();
-        $b->foo = 'bar';
-        dc_slack()->title('THIS IS OBJECT')->object($b);
+        dc_slack()->debug('message: ddd');
     }
 }
