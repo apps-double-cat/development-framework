896846c010c2bfb75cefaaef0a22777ee4d3b7a4
Khanh Nguyen <khanhdaik134@gmail.com>
2021-09-28 21:48:05 +0700

    update reset quota method

--- app/Http/Controllers/DevelopmentController.php
+++ app/Http/Controllers/DevelopmentController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use DoubleC\LaravelShopify\Services\SubscriptionHistoryService\SubscriptionHistoryService;
+use DoubleC\LaravelShopify\Traits\InstalledShop;
 use DoubleC\LaravelShopify\Traits\ShopSubscription;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
@@ -10,11 +12,12 @@ use Illuminate\Contracts\View\View;
 class DevelopmentController extends Controller
 {
     use ShopSubscription;
+    use InstalledShop;
 
     /**
      * @return Application|Factory|View
      */
-    public function index(): Factory|View|Application
+    public function index(SubscriptionHistoryService $subscriptionHistoryService)
     {
         return view('welcome');
     }
